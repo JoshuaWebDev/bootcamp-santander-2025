23e325d6f4cce51a00efdb592cd3c813a48a6e77
Josué Barros da Silva <josue.barros1986@gmail.com>
2025-07-25 02:24:09 -0300

    control structures

---
+++ getting-started/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        System.out.print("Informe seu nome: ");
+        var name = scanner.next();
+        System.out.print("Informe sua idade: ");
+        var age = scanner.nextInt();
+        System.out.print("Você é emancipado? (S/N): ");
+        var isEmancipated = scanner.next().equalsIgnoreCase("S");
+
+        var canDrive = (age >= 18) || (age >= 16 && isEmancipated);
+        var message = "";
+        if (canDrive) {
+            message = name +", você pode dirigir.";
+        } else {
+            message = name +", você não pode dirigir.";
+        }
+
+        System.out.println(message);
+    }
+}
