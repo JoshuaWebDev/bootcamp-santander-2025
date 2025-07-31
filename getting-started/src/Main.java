39a107c7d278463e01cfa300bebdd319d4c99919
Josué Barros da Silva <josue.barros1986@gmail.com>
2025-07-30 22:09:16 -0300

    switch case with arrow labels

--- getting-started/src/Main.java
+++ getting-started/src/Main.java
@@ -6,29 +6,13 @@ public class Main {
         System.out.println("Informe um número de 1 a 7: ");
         var option = scanner.nextInt();
         switch (option) {
-            case 1:
-                System.out.println("Domingo");
-                break;
-            case 2:
-                System.out.println("Segunda");
-                break;
-            case 3:
-                System.out.println("Terça");
-                break;
-            case 4:
-                System.out.println("Quarta");
-                break;
-            case 5:
-                System.out.println("Quinta");
-                break;
-            case 6:
-                System.out.println("Sexta");
-                break;
-            case 7:
-                System.out.println("Sábado");
-                break;
-            default:
-                System.out.println("Opção Invalida");
+            case 1, 7  -> System.out.println("Fim de Semana");
+            case 2  -> System.out.println("Segunda");
+            case 3  -> System.out.println("Terça");
+            case 4  -> System.out.println("Quarta");
+            case 5  -> System.out.println("Quinta");
+            case 6  -> System.out.println("Sexta");
+            default -> System.out.println("Opção Invalida");
         }
     }
 }
