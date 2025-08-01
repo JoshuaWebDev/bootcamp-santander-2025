5f53de0e3c1b009e1860a425000c7ee91b60ee79
Josué Barros da Silva <josue.barros1986@gmail.com>
2025-08-01 01:16:57 -0300

    laço for (mais antigo)

--- getting-started/src/Main.java
+++ getting-started/src/Main.java
@@ -4,11 +4,8 @@ public class Main {
 
     public static void main (String[] args) {
         var scanner = new Scanner(System.in);
-        for(;;) {
-            System.out.println("Digite um nome: ");
-            var name = scanner.next();
-            if (name.equalsIgnoreCase("exit")) break;
-            System.out.println(name);
+        for(var i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
         }
     }
 
