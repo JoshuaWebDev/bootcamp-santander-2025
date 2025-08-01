fe96ee705d5a188b9fd8842e4c1064ed2e6d1187
Josué Barros da Silva <josue.barros1986@gmail.com>
2025-08-01 01:06:57 -0300

    laço de repetição com sentinela

--- getting-started/src/Main.java
+++ getting-started/src/Main.java
@@ -1,19 +1,15 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+
+    public static void main (String[] args) {
         var scanner = new Scanner(System.in);
-        System.out.println("Informe um número de 1 a 7: ");
-        var option = scanner.nextInt();
-        var message = switch (option) {
-            case 1, 7  -> "Fim de Semana";
-            case 2  -> "Segunda";
-            case 3  -> "Terça";
-            case 4  -> "Quarta";
-            case 5  -> "Quinta";
-            case 6  -> "Sexta";
-            default -> "Opção Invalida";
-        };
-        System.out.println(message);
+        for(;;) {
+            System.out.println("Digite um nome: ");
+            var name = scanner.next();
+            if (name.equalsIgnoreCase("exit")) break;
+            System.out.println(name);
+        }
     }
+
 }
